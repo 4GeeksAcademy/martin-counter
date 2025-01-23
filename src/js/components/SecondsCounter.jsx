43341a922f73cfbb7b8a44586363aada6caed172
martinSabatini <146122086+martinSabatini@users.noreply.github.com>
2025-01-23 00:21:10 +0000

    CounterMartin

---
+++ src/js/components/SecondsCounter.jsx
@@ -0,0 +1,49 @@
+
+import React from 'react';
+
+
+
+
+const Digit = ({ value }) => {
+  return <span className="digit">{value}</span>;
+};
+
+// Definición del componente Counter
+const Counter = ({ segundos }) => {
+  const digits = segundos.toString().padStart(6, '0').split('');
+
+  return (
+    <div className="counter">
+      {digits.map((digit, index) => (
+        <Digit key={index} value={digit} />
+      ))}
+    </div>
+  );
+};
+
+// Componente principal de la aplicación
+const SecondsCounter = ({ segundosIniciales = 0 }) => {
+  const [segundos, setSegundos] = React.useState(segundosIniciales);
+
+  const incrementarSegundos = () => {
+    setSegundos((prevSegundos) => {
+      const newSegundos = prevSegundos + 1;
+      if (newSegundos > 999999) {
+        return 0;
+      }
+      return newSegundos;
+    });
+  };
+
+  React.useEffect(() => {
+    const interval = setInterval(incrementarSegundos, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <Counter segundos={segundos} />
+    </div>
+  );
+};
+export default SecondsCounter
